74f5a63c7848ff620052ea2dca3335b2128619f8
Starbin <starbin.onetek@gmail.com>
2026-09-18 13:41:17 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 import os
 
@@ -35,5 +34,11 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
 
+
+    
